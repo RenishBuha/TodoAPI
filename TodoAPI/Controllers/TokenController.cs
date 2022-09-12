15efa599b5720a32c345d3b7535718d86f0e4e24
Renish Buha <Renish.patel11@gmail.com>
2022-09-12 18:28:20 +0530

    Change connection string.

--- TodoAPI/Controllers/TokenController.cs
+++ TodoAPI/Controllers/TokenController.cs
@@ -51,7 +51,7 @@ namespace TodoAPI.Controllers
                         _configuration["Jwt:Issuer"],
                         _configuration["Jwt:Audience"],
                         claims,
-                        expires: DateTime.UtcNow.AddMinutes(10),
+                        expires: DateTime.UtcNow.AddHours(2),
                         signingCredentials: signIn);
 
                     return Ok(new JwtSecurityTokenHandler().WriteToken(token));
